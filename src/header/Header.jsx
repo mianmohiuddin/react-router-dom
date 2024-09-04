dd3206c7de6f87acbe77829778dd65d6f55696cf
mianmohiuddin <mianmohiuddin125@gmail.com>
2024-09-04 15:33:41 +0500

    working on Header.jsx

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
             <Link  to="#"
             className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                     >
-           Get started</Link>
+           Get started Routing</Link>
         </div>
             <div
              className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
